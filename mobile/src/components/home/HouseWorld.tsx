b41de5523de3028750d7e127f5fcc7c543a4c4a0
jpearleverett <jpearleverett@gmail.com>
2026-02-09 06:39:21 +0900

    Adjust skyBackground dimensions for better scaling

--- mobile/src/components/home/HouseWorld.tsx
+++ mobile/src/components/home/HouseWorld.tsx
@@ -841,10 +841,10 @@ const styles = StyleSheet.create({
   // Vertical pan adds up to ±150px in content coords. 3x height covers all cases.
   skyBackground: {
     position: 'absolute',
-    top: -SCREEN_HEIGHT * 0.1,
-    left: -SCREEN_WIDTH * 0.1,
-    width: SCREEN_WIDTH * 1.2,
-    height: SCREEN_HEIGHT * 1.2,
+    top: -SCREEN_HEIGHT * 0.15,
+    left: -SCREEN_WIDTH * 0.15,
+    width: SCREEN_WIDTH * 1.3,
+    height: SCREEN_HEIGHT * 1.3,
     zIndex: -1,
   },
   // Clouds - fixed to screen
