7acb01f9f21d468180e74e392ed7859cacec1627
jpearleverett <jpearleverett@gmail.com>
2026-02-08 23:37:32 +0900

    Adjust skyBackground positioning and size

--- mobile/src/components/home/HouseWorld.tsx
+++ mobile/src/components/home/HouseWorld.tsx
@@ -841,9 +841,9 @@ const styles = StyleSheet.create({
   // Vertical pan adds up to ±150px in content coords. 3x height covers all cases.
   skyBackground: {
     position: 'absolute',
-    top: -SCREEN_HEIGHT * 0.4,
-    left: -SCREEN_WIDTH * 0.25,
-    width: SCREEN_WIDTH * 1.5,
+    top: -SCREEN_HEIGHT * 0.8,
+    left: -SCREEN_WIDTH * 0.35,
+    width: SCREEN_WIDTH * 1.7,
     height: SCREEN_HEIGHT * 2.2,
     zIndex: -1,
   },
