9bae0aa812f3b93e36945abf3dd2b4dacce40582
jpearleverett <jpearleverett@gmail.com>
2026-03-10 01:08:57 -0400

    Adjust sky background position for better visibility

--- mobile/src/components/home/HouseWorld.tsx
+++ mobile/src/components/home/HouseWorld.tsx
@@ -911,7 +911,7 @@ export const HouseWorld: React.FC<HouseWorldProps> = ({
                   SKY_DAY
                 }
                 style={[styles.skyBackground, {
-                  top: -Math.max(SCREEN_HEIGHT * 0.10, houseHeight * 0.0),
+                  top: -Math.max(SCREEN_HEIGHT * 0.50, houseHeight * 0.0),
                 }]}
                 resizeMode="cover"
               />
