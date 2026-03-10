ca3c3dcf207d651e4ce16c28e4610e48077ed0cc
jpearleverett <jpearleverett@gmail.com>
2026-03-09 21:15:46 -0400

    Fix sky background top position calculation

--- mobile/src/components/home/HouseWorld.tsx
+++ mobile/src/components/home/HouseWorld.tsx
@@ -1116,7 +1116,7 @@ const styles = StyleSheet.create({
   // Sky background - moves with scene, oversized to prevent gaps during pan.
   skyBackground: {
     position: 'absolute',
-    top: -SCREEN_HEIGHT * -0.5,
+    top: -SCREEN_HEIGHT * 0.5,
     left: -SCREEN_WIDTH * 0,
     width: SCREEN_WIDTH * 1,
     height: SCREEN_HEIGHT * 1,
